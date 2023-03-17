c3fe028b92246ad0cd5cd988480c4114e96dee19
mustafaensar <mustafaensar55@gmail.com>
2023-03-17 21:29:45 +0100

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { nanoid } from 'nanoid';
+
+export default function Todo() {
+
+  const [notes,setNotes] = React.useState(() => JSON.parse(localStorage.getItem("notes")) || []);
+  const [note, setNewNote] = React.useState("");
+
+  
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  },[notes])
+  
+
+  function createNewNote(){
+
+    if(!note){
+      alert("Please write a note");
+      return;
+    }
+
+    const newNote = {
+      id: nanoid(),
+      body: note,
+    }
+
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+  }
+
+  function deleteNote(id){
+    const newNotes = notes.filter(elem => elem.id !==id);
+    setNotes(newNotes);
+  }
+
+
+
+  return (
+    <>
+    <div className='todo-app'>
+      <div className='input-area'>
+        <input
+          type="text"
+          placeholder='Write Todo'
+          value={note}
+          onChange={ e => setNewNote(e.target.value)}
+        />
+        <button onClick={() => createNewNote()}>Add</button>
+      </div>
+      <div className='todo-list'>
+        <ul>
+          {notes.map(note => {
+            return(
+              <li key={note.id}>{note.body} <button onClick={() => deleteNote(note.id)}>Delete</button> </li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+    </>
+  )
+}
