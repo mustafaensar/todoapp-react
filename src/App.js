498216b21799d9c60d54d9dad54fb370ad6817cb
mustafaensar <mustafaensar55@gmail.com>
2023-03-21 02:30:15 +0100

    props are included

--- src/App.js
+++ src/App.js
@@ -1,14 +1,97 @@
-import './App.css';
+import React from 'react';
 import Header from './components/Header';
 import Todo from './components/Todo';
+import { nanoid } from 'nanoid';
 import "@fontsource/rubik";
 import "@fontsource/inter";
+import './App.css';
+
+
 
 function App() {
+
+  const [notes,setNotes] = React.useState(() => JSON.parse(localStorage.getItem("notes")) || []);
+  const [note, setNewNote] = React.useState("");
+  
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+    setNewNote("");
+  },[notes])
+  
+
+  function createNewNote(){
+
+    if(!note){
+      alert("Please write a note");
+      return;
+    }
+
+    const newNote = {
+      id: nanoid(),
+      body: note,
+      seen: false
+    }
+
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+  }
+
+  function deleteNote(id){
+    setNotes((prevNotes) => prevNotes.filter(elem => elem.id !==id));
+  }
+  
+  function updateNote(id){
+    let promptNote = prompt("Write a new note")
+    
+    const index = notes.findIndex(obj => {
+      return obj.id === id;
+    });
+
+    const newNotes = [...notes];
+    newNotes[index].body = promptNote;
+    setNotes(newNotes);
+  }
+
+  function handleCheckBox(id){
+    
+    const index = notes.findIndex(obj => {
+      return obj.id === id;
+    });
+    
+    const newNotes = [...notes];
+    if(!newNotes[index].seen){
+      newNotes[index].seen = true;
+      setNotes(newNotes);
+    }else{
+      newNotes[index].seen = false;
+      setNotes(newNotes);
+    }
+  }
+
+  function countSelectedNotes(){
+    const newNotes = notes.filter(elem => elem.seen === true);
+    return newNotes.length;
+  }
+
+  function deleteCheckedNotes(){
+    if(countSelectedNotes !== 0){
+      setNotes((prevNotes) => prevNotes.filter(elem => elem.seen !== true));
+    }
+  }
+
   return (
     <div className="App">
       <Header/>
-      <Todo/>
+      <Todo
+        note = {note}
+        notes = {notes}
+        setNewNote = {setNewNote}
+        createNewNote = {createNewNote}
+        deleteNote = {deleteNote}
+        updateNote = {updateNote}
+        handleCheckBox = {handleCheckBox}
+        countSelectedNotes ={countSelectedNotes}
+        deleteCheckedNotes = {deleteCheckedNotes}
+      />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,77 +1,5 @@
 import React from 'react'
-import { nanoid } from 'nanoid';
-
-export default function Todo() {
-
-  const [notes,setNotes] = React.useState(() => JSON.parse(localStorage.getItem("notes")) || []);
-  const [note, setNewNote] = React.useState("");
-  
-  React.useEffect(() => {
-    localStorage.setItem("notes", JSON.stringify(notes));
-    setNewNote("");
-  },[notes])
-  
-
-  function createNewNote(){
-
-    if(!note){
-      alert("Please write a note");
-      return;
-    }
-
-    const newNote = {
-      id: nanoid(),
-      body: note,
-      seen: false
-    }
-
-    setNotes((prevNotes) => [newNote, ...prevNotes]);
-  }
-
-  function deleteNote(id){
-    const newNotes = notes.filter(elem => elem.id !==id);
-    setNotes(newNotes);
-  }
-  
-  function updateNote(id){
-    let promptNote = prompt("Write a new note")
-    
-    const index = notes.findIndex(obj => {
-      return obj.id === id;
-    });
-
-    const newNotes = [...notes];
-    newNotes[index].body = promptNote;
-    setNotes(newNotes);
-  }
-
-  function handleCheckBox(id){
-    
-    const index = notes.findIndex(obj => {
-      return obj.id === id;
-    });
-    
-    const newNotes = [...notes];
-    if(!newNotes[index].seen){
-      newNotes[index].seen = true;
-      setNotes(newNotes);
-    }else{
-      newNotes[index].seen = false;
-      setNotes(newNotes);
-    }
-  }
-
-  function countSelectedNotes(){
-    const newNotes = notes.filter(elem => elem.seen === true);
-    return newNotes.length;
-  }
-
-  function deleteCheckedNotes(){
-    if(countSelectedNotes !== 0){
-      const newNotes = notes.filter(elem => elem.seen !== true);
-      setNotes(newNotes);
-    }
-  }
+export default function Todo(props) {
 
   return (
     <>
@@ -80,14 +8,14 @@ export default function Todo() {
         <input
           type="text"
           placeholder='Write Todo'
-          value={note}
-          onChange={ e => setNewNote(e.target.value)}
+          value={props.note}
+          onChange={ e => props.setNewNote(e.target.value)}
         />
-        <button className='button' onClick={() => createNewNote()}>Add</button>
+        <button className='button' onClick={() => {props.createNewNote()}}>Add</button>
       </div>
       <div className='todo-list'>
         <ul>
-          {notes.map(note => {
+          {props.notes.map(note => {
             return(
               <li key={note.id}>
                 <div>
@@ -95,13 +23,13 @@ export default function Todo() {
                   className='checkbox-round'
                   type="checkbox"
                   checked={note.seen}
-                  onChange={() => handleCheckBox(note.id)}
+                  onChange={() => {props.handleCheckBox(note.id)}}
                 />
                 </div>
                 {note.body}
                 <div className='list--button'>
-                <button className='button' onClick={() => updateNote(note.id)}>Update</button>  
-                <button className='button' onClick={() => deleteNote(note.id)}>Delete</button>    
+                <button className='button' onClick={() => {props.updateNote(note.id)}}>Update</button>  
+                <button className='button' onClick={() => {props.deleteNote(note.id)}}>Delete</button>    
                 </div> 
               </li>
             )
@@ -110,8 +38,8 @@ export default function Todo() {
       </div>
       <hr/>
       <div className='footer'>
-        <p><span>{countSelectedNotes()}</span> item selected</p>
-        <p className='clear-all' onClick={deleteCheckedNotes}>Clear All</p>
+        <p><span>{props.countSelectedNotes()}</span> item selected</p>
+        <p className='clear-all' onClick={() => {props.deleteCheckedNotes()}}>Clear All</p>
       </div>
     </div>
     </>
